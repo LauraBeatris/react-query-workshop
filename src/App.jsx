bb6acf466795f43602aa3006a34aa91bfb96e849
Laura Beatris <48022589+LauraBeatris@users.noreply.github.com>
2022-05-31 20:31:39 +0200

    Fetch repositories using fetch API

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,24 @@
+import { useEffect, useState } from 'react'
+
 function App () {
+  const [repositories, setRepositories] = useState([])
+
+  useEffect(() => {
+    fetch('https://api.github.com/users/LauraBeatris/repos')
+      .then((response) => response.json())
+      .then((data) => setRepositories(data))
+  }, [])
+
   return (
-    <div className='App' />
+    <ul className='App'>
+      {repositories?.map((item) => (
+        <li key={item.id}>
+          <strong>{item.full_name}</strong>
+          <br />
+          <small>{item.description}</small>
+        </li>
+      ))}
+    </ul>
   )
 }
 
