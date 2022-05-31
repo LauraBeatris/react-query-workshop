5289c0ea88a3eaf42cdbe636c9d48ded54325e6d
Laura Beatris <48022589+LauraBeatris@users.noreply.github.com>
2022-05-31 21:16:27 +0200

    Configuring API calls with react-query

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { QueryClientProvider } from 'react-query'
+
 import App from './App'
+import { queryCLient } from './config'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryCLient}>
+      <App />
+    </QueryClientProvider>
   </React.StrictMode>
 )
