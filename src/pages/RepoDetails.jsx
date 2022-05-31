17336fdb6be6e74d962d22fe086c9651b4d7db1a
Laura Beatris <48022589+LauraBeatris@users.noreply.github.com>
2022-05-31 23:15:17 +0200

    Extract query to separate hook

--- src/pages/RepoDetails.jsx
+++ src/pages/RepoDetails.jsx
@@ -7,26 +7,17 @@ function getRepositoryByName (name) {
   return githubAPI.get(`/repos/${name}`).then(res => res.data)
 }
 
-export function RepoDetails () {
+export function useRepositoryQuery () {
   const fullName = useParams()['*']
   const queryClient = useQueryClient()
 
   const repositoriesListCache = queryClient.getQueryData('repos')
   const currentRepositoryCache = repositoriesListCache?.find((repo) => repo.full_name === fullName)
-  const { data } = useQuery(`repo-${fullName}`, () => getRepositoryByName(fullName), {
+  const { data, isFetching } = useQuery(`repo-${fullName}`, () => getRepositoryByName(fullName), {
     enabled: !currentRepositoryCache
   })
 
-  const [description, setDescription] = useState('')
-
-  const handleInputChange = (event) => {
-    setDescription(event.target.value)
-  }
-
-  const handleDescriptionUpdate = (event) => {
-    event.preventDefault()
-    setDescription('')
-
+  const updateCacheWithDescription = (description) => {
     if (!repositoriesListCache) return
 
     const updatedCachedList = repositoriesListCache.map((repo) => {
@@ -37,21 +28,45 @@ export function RepoDetails () {
     queryClient.setQueryData('repos', updatedCachedList)
   }
 
+  return { data, isFetching, updateCacheWithDescription }
+}
+
+export function RepoDetails () {
+  const { data, isFetching, updateCacheWithDescription } = useRepositoryQuery()
+  const [description, setDescription] = useState('')
+
+  const handleInputChange = (event) => {
+    setDescription(event.target.value)
+  }
+
+  const handleDescriptionUpdate = (event) => {
+    event.preventDefault()
+    setDescription('')
+
+    updateCacheWithDescription(description)
+  }
+
   return (
     <div>
       <Link to='/'>Back to main page</Link>
 
-      <p>{data?.full_name}</p>
+      {isFetching
+        ? <p>Loading...</p>
+        : (
+          <section>
+            <p>{data?.full_name}</p>
 
-      <form onSubmit={handleDescriptionUpdate}>
-        <input
-          type='text'
-          value={description}
-          onChange={handleInputChange}
-          placeholder='New repo description'
-        />
-        <button type='submit'>Update description</button>
-      </form>
+            <form onSubmit={handleDescriptionUpdate}>
+              <input
+                type='text'
+                value={description}
+                onChange={handleInputChange}
+                placeholder='New repo description'
+              />
+              <button type='submit'>Update description</button>
+            </form>
+          </section>
+          )}
     </div>
   )
 }
